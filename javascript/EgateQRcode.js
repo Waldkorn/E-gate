381f1e46157a191f7b641b4fb088bfd013265956
Esmeralda <34519178+Esmoves@users.noreply.github.com>
2018-01-07 08:38:31 +0100

    Add files via upload

--- javascript/EgateQRcode.js
+++ javascript/EgateQRcode.js
@@ -1,5 +1,5 @@
 function QRcode(outcome) {
 	if (outcome == true) {
-		document.getElementById("QRelement").innerHTML = "<img src=Images/QRHarambe.jpg>";
+		document.getElementById("reserveer").innerHTML = "<h1>Use this code to enter the parkinglot</h1><img src=Images/QRHarambe.jpg />";
 	}
 }
